511c96a629772b2cb6a87ff7e5af9cee176e79a8
Mohamed <sulimmoh@gmail.com>
2022-10-09 03:32:19 -0500

    created a new cluster and a bucket and RDS dueo to consumning the freetier budget

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://abbafd1d9259d463689d41168f0723a2-1445108855.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a9ded4482b3454bf5a6faedd175d4804-90174561.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
